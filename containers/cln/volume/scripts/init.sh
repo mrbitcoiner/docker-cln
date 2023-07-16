511b4a101024d5e1154da10ff606d6c36d82b4bc
mrbitcoiner <mrbitcoiner@tutanota.com>
2023-07-16 18:22:04 +0000

    * Control and Init Scripts   - Refactor   - Added option to extend the configurations inside scripts/custom directory

--- containers/cln/volume/scripts/init.sh
+++ containers/cln/volume/scripts/init.sh
@@ -12,4 +12,13 @@ su -c '/app/scripts/cln_setup.sh' ${CONTAINER_USER}
 /app/scripts/shutdown.sh
 su -c '/app/scripts/rpc_expose.sh' ${CONTAINER_USER}
 
+if [ -e /app/scripts/custom/init.sh ]; then
+  su -c '/app/scripts/custom/init.sh' ${CONTAINER_USER}
+fi
+
+if [ -e /app/data/PATH.env ]; then
+  source /app/data/PATH.env; 
+  export PATH=${PATH}
+fi
+
 su -c 'lightningd' ${CONTAINER_USER}
--- control.sh
+++ control.sh
@@ -3,20 +3,38 @@
 set -e
 ####################
 chmod +x scripts/*.sh
-get_env(){ 
-  scripts/get_env.sh '.env' "${1}" 
-}
 ####################
-readonly "$(get_env 'CLN_CONTAINER_NAME')"
+source .env
 readonly CONTAINER_NAMES=("${CLN_CONTAINER_NAME}")
 readonly CLN_CONTAINER='cln'  
 readonly CONTAINERS=("${CLN_CONTAINER}")
-readonly "$(get_env 'NETWORK')"
 ####################
 print_err(){
   if [ -z "${1}" ]; then printf 'Expected: [msg]\n' 1>&2; return 1; fi
   printf "${1}\n" 1>&2
   return 1
+}
+check_env(){
+  if [ -z ${CLN_CONTAINER_NAME} ]; then printf 'Undefined env CLN_CONTAINER_NAME\n' 1>&2; return 1; fi
+  if [ -z ${NETWORK} ]; then printf 'Undefined env NETWORK\n' 1>&2; return 1; fi
+  if [ -z ${BITCOIN_CLI_PATH} ]; then printf 'Undefined env BITCOIN_CLI_PATH\n' 1>&2; return 1; fi
+  if [ -z ${CLN_EXPOSE_RPC} ]; then printf 'Undefined env CLN_EXPOSE_RPC\n' 1>&2; return 1; fi
+  if [ -z ${CLN_INT_RPC_PORT} ]; then printf 'Undefined env CLN_INT_RPC_PORT\n' 1>&2; return 1; fi
+  if [ -z ${CLN_EXT_RPC_PORT} ]; then printf 'Undefined env CLN_EXT_RPC_PORT\n' 1>&2; return 1; fi
+  if [ -z ${BITCOIN_RPC_HOSTNAME} ]; then printf 'Undefined env BITCOIN_RPC_HOSTNAME\n' 1>&2; return 1; fi
+  if [ -z ${BITCOIN_RPC_PORT} ]; then printf 'Undefined env BITCOIN_RPC_PORT\n' 1>&2; return 1; fi
+  if [ -z ${BITCOIN_RPC_USERNAME} ]; then printf 'Undefined env BITCOIN_RPC_USERNAME\n' 1>&2; return 1; fi
+  if [ -z ${BITCOIN_RPC_PASSWORD} ]; then printf 'Undefined env BITCOIN_RPC_PASSWORD\n' 1>&2; return 1; fi
+  if [ -z ${CLN_NETWORK} ]; then printf 'Undefined env CLN_NETWORK\n' 1>&2; return 1; fi
+  if [ -z ${CLN_ALIAS} ]; then printf 'Undefined env CLN_ALIAS\n' 1>&2; return 1; fi
+  if [ -z ${CLN_TRUSTEDCOIN_PLUGIN} ]; then printf 'Undefined env CLN_TRUSTEDCOIN_PLUGIN\n' 1>&2; return 1; fi
+  if [ -z ${CLN_BASE_FEE_MSAT} ]; then printf 'Undefined env CLN_BASE_FEE_MSAT\n' 1>&2; return 1; fi
+  if [ -z ${CLN_PPM_FEE} ]; then printf 'Undefined env CLN_PPM_FEE\n' 1>&2; return 1; fi
+  if [ -z ${CLN_MIN_CH_CAPACITY_SAT} ]; then printf 'Undefined env CLN_MIN_CH_CAPACITY_SAT\n' 1>&2; return 1; fi
+  if [ -z ${CLN_MAX_HTLC_INFLIGHT} ]; then printf 'Undefined env CLN_MAX_HTLC_INFLIGHT\n' 1>&2; return 1; fi
+  if [ -z ${CLN_MAX_HTLC_SIZE_MSAT} ]; then printf 'Undefined env CLN_MAX_HTLC_SIZE_MSAT\n' 1>&2; return 1; fi
+  if [ -z ${CLN_MIN_HTLC_SIZE_MSAT} ]; then printf 'Undefined env CLN_MIN_HTLC_SIZE_MSAT\n' 1>&2; return 1; fi
+  if [ -z ${TOR_PROXY} ]; then printf 'Undefined env TOR_PROXY\n' 1>&2; return 1; fi
 }
 setup_directories(){
   for i in ${CONTAINERS[@]}; do
@@ -29,41 +47,35 @@ set_script_permissions(){
     if [ -e ./containers/${i}/volume/scripts/init.sh ]; then
       chmod +x ./containers/${i}/volume/scripts/*.sh
     fi
+    if [ -e ./containers/${i}/volume/scripts/custom/init.sh ]; then
+      chmod +x ./containers/${i}/volume/scripts/custom/*.sh
+    fi
   done
-}
-env_set(){
-  if [ -z "${1}" ] || [ -z "${2}" ]; then print_err "Expected: [ key ] [ value ]"; fi
-  local key="${1}"
-  local value="${2}"
-  local FILE='.env'
-  scripts/set_dotenv.sh "${FILE}" "${key}" "${value}"
-}
-set_user_env(){
-  env_set "CONTAINER_UID" "$(id -u)"
-  env_set "CONTAINER_GID" "$(id -g)"
-  env_set "CONTAINER_USER" "${USER}"
+  if [ -e ./scripts/custom/init.sh ]; then
+    chmod +x ./scripts/custom/*.sh
+  fi
 }
 build_images(){
   docker-compose build \
-    --build-arg $(get_env 'CONTAINER_USER') \
-    --build-arg $(get_env 'CONTAINER_UID') \
-    --build-arg $(get_env 'CONTAINER_GID') \
-    --build-arg $(get_env 'BITCOIN_RPC_HOSTNAME') \
-    --build-arg $(get_env 'BITCOIN_RPC_PORT') \
-    --build-arg $(get_env 'BITCOIN_RPC_USERNAME') \
-    --build-arg $(get_env 'BITCOIN_RPC_PASSWORD') \
-    --build-arg $(get_env 'CLN_NETWORK') \
-    --build-arg $(get_env 'CLN_ALIAS') \
-    --build-arg $(get_env 'CLN_TRUSTEDCOIN_PLUGIN') \
-    --build-arg $(get_env 'CLN_EXPOSE_RPC') \
-    --build-arg $(get_env 'CLN_INT_RPC_PORT') \
-    --build-arg $(get_env 'CLN_BASE_FEE_MSAT') \
-    --build-arg $(get_env 'CLN_PPM_FEE') \
-    --build-arg $(get_env 'CLN_MIN_CH_CAPACITY_SAT') \
-    --build-arg $(get_env 'CLN_MAX_HTLC_INFLIGHT') \
-    --build-arg $(get_env 'CLN_MAX_HTLC_SIZE_MSAT') \
-    --build-arg $(get_env 'CLN_MIN_HTLC_SIZE_MSAT') \
-    --build-arg $(get_env 'TOR_PROXY')
+    --build-arg CONTAINER_USER=${USER} \
+    --build-arg CONTAINER_UID=$(id -u) \
+    --build-arg CONTAINER_GID=$(id -g) \
+    --build-arg BITCOIN_RPC_HOSTNAME=${BITCOIN_RPC_HOSTNAME} \
+    --build-arg BITCOIN_RPC_PORT=${BITCOIN_RPC_PORT} \
+    --build-arg BITCOIN_RPC_USERNAME=${BITCOIN_RPC_USERNAME} \
+    --build-arg BITCOIN_RPC_PASSWORD=${BITCOIN_RPC_PASSWORD} \
+    --build-arg CLN_NETWORK=${CLN_NETWORK} \
+    --build-arg CLN_ALIAS=${CLN_ALIAS} \
+    --build-arg CLN_TRUSTEDCOIN_PLUGIN=${CLN_TRUSTEDCOIN_PLUGIN} \
+    --build-arg CLN_EXPOSE_RPC=${CLN_EXPOSE_RPC} \
+    --build-arg CLN_INT_RPC_PORT=${CLN_INT_RPC_PORT} \
+    --build-arg CLN_BASE_FEE_MSAT=${CLN_BASE_FEE_MSAT} \
+    --build-arg CLN_PPM_FEE=${CLN_PPM_FEE} \
+    --build-arg CLN_MIN_CH_CAPACITY_SAT=${CLN_MIN_CH_CAPACITY_SAT} \
+    --build-arg CLN_MAX_HTLC_INFLIGHT=${CLN_MAX_HTLC_INFLIGHT} \
+    --build-arg CLN_MAX_HTLC_SIZE_MSAT=${CLN_MAX_HTLC_SIZE_MSAT} \
+    --build-arg CLN_MIN_HTLC_SIZE_MSAT=${CLN_MIN_HTLC_SIZE_MSAT} \
+    --build-arg TOR_PROXY=${TOR_PROXY}
 }
 create_network(){
   if ! docker network ls | grep "${NETWORK}" > /dev/null; then
@@ -76,10 +88,6 @@ start_containers(){
     &
 }
 copy_bitcoin_cli(){
-  if ! get_env "BITCOIN_CLI_PATH" > /dev/null; then
-    return 0
-  fi
-  local $(get_env "BITCOIN_CLI_PATH")
   local destination=./containers/${CLN_CONTAINER}/volume/data/bitcoin
   if ! [ -e ${destination}/bitcoin-cli ]; then
     mkdir -p ${destination}
@@ -87,13 +95,11 @@ copy_bitcoin_cli(){
   fi
 }
 generate_docker_compose(){
-  local $(get_env "CLN_INT_RPC_PORT")
-  local $(get_env "CLN_EXT_RPC_PORT")
   cat << EOF > docker-compose.yml
 services:
   cln:
     container_name: ${CLN_CONTAINER_NAME} 
-    build: ./containers/${CLN_CONTAINER}
+    build: ./containers/cln
     volumes:
       - ./containers/cln/volume:/app
     ports:
@@ -107,14 +113,20 @@ networks:
     external: true
 EOF
 }
+run_custom(){
+  if [ -e ./scripts/custom/init.sh ]; then
+    ./scripts/custom/init.sh
+  fi
+}
 ####################
 boot(){
+  check_env
   create_network
   generate_docker_compose
   setup_directories
   set_script_permissions
-  set_user_env
   copy_bitcoin_cli
+  run_custom
   build_images
   start_containers
 }
@@ -137,14 +149,12 @@ cli_wrapper(){
   docker exec -it ${CLN_CONTAINER_NAME} su -c 'lightning-cli '"${command}"'' ${USER}
 }
 create_cln_socket(){
-  local $(get_env "CLN_EXPOSE_RPC")
   if ! echo "${CLN_EXPOSE_RPC}" | grep '^enabled$' 1> /dev/null; then return 0; fi
-  local $(get_env "CLN_EXT_RPC_PORT")
   socat UNIX-LISTEN:cln.sock,fork,reuseaddr TCP:127.0.0.1:${CLN_EXT_RPC_PORT} &
   echo "$!" > sock.pid
 }
 stop_cln_socket(){
-  if ! [ -e sock.pid ]; then print_err "Socket does not exist"; fi
+  if ! [ -e sock.pid ]; then printf 'Socket does not exist\' 1>&2; return 1; fi
   let pid="$(cat sock.pid)"
   if kill -0 ${pid}; then
     kill -2 ${pid}
@@ -160,5 +170,5 @@ case ${1} in
   sock_forward) create_cln_socket ;;
   sock_stop) stop_cln_socket ;;
   nop) ;;
-  *) print_err 'Expected: [ up | down | sock_forward | sock_stop | cli_wrapper | clean ]' ;;
+  *) printf 'Expected: [ up | down | sock_forward | sock_stop | cli_wrapper | clean ]\n' 1>&2; return 1 ;;
 esac
